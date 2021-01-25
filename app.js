825dd0fdf65d7d3008579d411dcdd5fc4f13afd9
THWinther <43626565+THWinther@users.noreply.github.com>
2021-01-25 15:04:18 +0000

    lint virkar og app.js lintað

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-
 import express from 'express';
 
+let viewsPath = new URL('./views', import.meta.url).pathname;
+const publicURL = new URL('./public', import.meta.url).pathname;
 const app = express();
 
 // Þetta verður aðgengilegt gegnum `local.bar` í template
-app.locals.importantize = str => `${str}!`;
+app.locals.importantize = (str) => (`${str}!`);
 
-const viewsPath = new URL('./views', import.meta.url).pathname;
-console.log(viewsPath);
-app.set('views', viewsPath);
+// URL bætti alltaf við auka / í byrjun þetta eyðir því
+viewsPath = viewsPath.substr(1, viewsPath.length);
 app.set('view engine', 'ejs');
+app.set('views', viewsPath);
+
+app.use(express.static(publicURL.substring(1, publicURL.length)));
 
 app.get('/', (req, res) => {
   // `title` verður aðgengilegt sem breyta í template
   res.render('index', { title: 'Forsíða' });
 });
-
 
 const hostname = '127.0.0.1';
 const port = 3000;
